37f6c2235ce559bc412edfd5448bf2e5e42c4828
Radaev Mikhail <msfrms@gmail.com>
2021-01-06 15:09:34 +0300

    #11 добавил список треков

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Trach/chart/chart.dart';
 import 'package:flutter/material.dart';
 
 import 'chart/track.dart';
@@ -74,49 +75,197 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    var chart = ChartWidget(
+      ChartProps(tracks: [
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        ),
+        TrackProps(
+            position: PositionProps(
+                value: "1",
+                change: PositionChangeProps(
+                    value: "2",
+                    change: PositionChange.decrement
+                )
+            ),
+            coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
+            title: "Kosandra",
+            artists: "Miyagi & Andy Panda"
+        )
+      ])
+    );
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TrackWidget(
-                TrackProps(
-                    position: PositionProps(
-                        value: "1",
-                        change: PositionChangeProps(
-                            value: "2",
-                            change: PositionChange.decrement
-                        )
-                    ),
-                    coverUrl: "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg",
-                    title: "Kosandra",
-                    artists: "Miyagi & Andy Panda"
-                )
-            )
-          ],
-        ),
-      ),
+      body: chart,
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
